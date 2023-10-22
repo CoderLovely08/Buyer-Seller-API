a7b752a565c57a5e83e7b0af82e519117a1c4152
Lovely Sharma <lovelysingeras297@gmail.com>
2023-10-22 17:20:38 +0530

    Added /users route for viewing all users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,29 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { registerUser, validateUser } from "../controllers/userController.js";
+import { getAllUsers } from "../modules/helperModule.js";
 
 const router = express.Router();
+
+router.route('/users')
+    .get(async (req, res) => {
+        try {
+            const result = await getAllUsers();
+            res.json({
+                result
+            })
+        } catch (error) {
+            // Return if the query fails
+            return {
+                success: false,
+                message: 'Internal Server Error',
+                error: error
+            }
+        }
+    })
 
 router.route('/register')
     .post(registerUser)
 
+router.route('/login')
+    .post(validateUser)
 export default router
